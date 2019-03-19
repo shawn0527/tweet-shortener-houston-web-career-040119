9b885fdf9900c0feb077ea5c321ab25bc893cd83
Shawn Ren <doggy-ren@hotmail.com>
2019-03-19 18:40:49 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -34,7 +34,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.delete("!.,? ").length > 140
+  if tweet.length > 140
     puts word_substituter(tweet)
   else
     puts tweet
