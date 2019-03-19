dfe271c6f40ba27db11236024a2240526b9cab41
Shawn Ren <doggy-ren@hotmail.com>
2019-03-19 18:34:59 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -34,7 +34,7 @@ def bulk_tweet_shortener(tweets)
 end
 
 def selective_tweet_shortener(tweet)
-  if tweet.size > 140
+  if tweet.length > 140
     puts word_substituter(tweet)
   else
     puts tweet
