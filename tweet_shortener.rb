f8a37441fa2075d6cd2a03124e62564bea788cf8
Shawn Ren <doggy-ren@hotmail.com>
2019-03-19 16:38:25 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,12 +1,12 @@
 def dictionary
   dictionary = {
-    "hello": => "hi",
-    "to, two, too": => "2",
-    "for, four": => "4",
-    "be": => "b",
-    "you": => "u",
-    "at": => "@",
-    "and": => "&"
+    "hello" => "hi",
+    "to, two, too" => "2",
+    "for, four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
   }
 end
 
